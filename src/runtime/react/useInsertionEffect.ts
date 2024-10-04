b85ada77eeadec32beb18850e98e5906766ea8ea
Arby Djabaev <contact@arby.be>
2024-10-04 18:37:36 +0200

    fix(react hooks): Add cleanup and deps to useInsertionEffect

--- src/runtime/react/useInsertionEffect.ts
+++ src/runtime/react/useInsertionEffect.ts
@@ -1,5 +1,29 @@
-export function useInsertionEffect(effect) {
+import type { DependencyList, EffectCallback } from '#motion/html'
+
+export function useInsertionEffect(effect: EffectCallback, deps: DependencyList = []) {
+  let cleanupFn
+
+  const runEffect = () => {
+    if (typeof cleanupFn === 'function') {
+      cleanupFn()
+    }
+
+    cleanupFn = effect()
+  }
+
   onBeforeMount(() => {
-    effect()
+    runEffect()
   })
+
+  onBeforeUnmount(() => {
+    if (typeof cleanupFn === 'function') {
+      cleanupFn()
+    }
+  })
+
+  if (deps.length > 0) {
+    watch(deps, () => {
+      runEffect()
+    }, { deep: true })
+  }
 }
