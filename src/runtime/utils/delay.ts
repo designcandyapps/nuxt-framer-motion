bacf226a9c11116438edbbcb89456bb60a22bdb8
Arby Djabaev <contact@arby.be>
2024-10-04 19:16:13 +0200

    upstream: Adding delay in seconds

--- src/runtime/utils/delay.ts
+++ src/runtime/utils/delay.ts
@@ -1,6 +1,7 @@
 import { cancelFrame, frame } from '#motion/frameloop'
 import { time } from '#motion/frameloop/syncTime'
 import type { FrameData } from '#motion/frameloop/types'
+import { secondsToMilliseconds } from '#motion/utils/timeConversion'
 
 export type DelayedFunction = (overshoot: number) => void
 
@@ -22,4 +23,8 @@ export function delay(callback: DelayedFunction, timeout: number) {
   frame.read(checkElapsed, true)
 
   return () => cancelFrame(checkElapsed)
+}
+
+export function delayInSeconds(callback: DelayedFunction, timeout: number) {
+  return delay(callback, secondsToMilliseconds(timeout))
 }
