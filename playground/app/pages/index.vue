8652e4bb0f4e1a83beb6ea15d7f9a8bed63c8c06
Erin Dunn Halligan <erin@designcandy.com>
2025-03-11 19:57:41 -0500

    Update index.vue

--- playground/app/pages/index.vue
+++ playground/app/pages/index.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts">
+<template>
+  <MotionMain
+    class="container"
+    :initial="{ backgroundSize: '100%' }"
+    :animate="{ backgroundSize: '200%' }"
+    :transition="{
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+      repeat: Infinity,
+      repeatType: 'mirror',
+      repeatDelay: 0.2
+    }"
+  />
+</template>
 
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.container {
+  width: 150px;
+  height: 150px;
+  background-image: url("https://framerusercontent.com/images/M4SNURkNYCDjIiopKdiL689jOQ.svg");
+  border-radius: 30px;
+}
+</style>
+
+
+
+
+<!--script setup lang="ts">
 </script>
 
 <template>
@@ -7,4 +37,4 @@
 </template>
 
 <style scoped>
-</style>
+</style-->
